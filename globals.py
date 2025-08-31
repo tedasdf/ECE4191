4b5604d9df131fa3bbee7c3d017728f032db0c51
Riley <rjoh0022@student.monash.edu>
2025-08-31 12:55:56 +1000

    Changed some formatting

--- globals.py
+++ globals.py
@@ -1,2 +1,3 @@
-video_url = "https://www3.cde.ca.gov/download/rod/big_buck_bunny.mp4"
+# video_url = "https://www3.cde.ca.gov/download/rod/big_buck_bunny.mp4"
+video_url = "udp://10.175.112.23:5000"
 # url = "0"
