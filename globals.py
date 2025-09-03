413911aea56cc1a6d3003345daeead4427a56a82
Shang Chen <sche0151@student.monash.edu>
2025-09-03 20:14:32 +1000

    updated global file

--- globals.py
+++ globals.py
@@ -1,4 +1,5 @@
 # video_url = "https://www3.cde.ca.gov/download/rod/big_buck_bunny.mp4"
+# pi_url = "http://192.168.77.1:7123"
 video_url = "udp://10.175.112.23:5000"
 audio_url = "http://10.175.112.23:8080"
 streaming = False
