a72dd74fa81849369b03e6e2e066c614983c30c5
Shang Chen <sche0151@student.monash.edu>
2025-09-05 00:29:58 +1000

    temporary update to global url links for testing

--- globals.py
+++ globals.py
@@ -1,7 +1,7 @@
-# video_url = "https://www3.cde.ca.gov/download/rod/big_buck_bunny.mp4"
-# audio_url = "https://samplelib.com/lib/preview/mp3/sample-9s.mp3"
+video_url = "https://www3.cde.ca.gov/download/rod/big_buck_bunny.mp4"
+audio_url = "https://samplelib.com/lib/preview/mp3/sample-9s.mp3"
 # pi_url = "http://192.168.77.1:7123/stream.mjpg"
-video_url = "udp://10.175.112.23:5000"
-audio_url = "http://10.175.112.23:8080"
+# video_url = "udp://10.175.112.23:5000"
+# audio_url = "http://10.175.112.23:8080"
 streaming = False
 capture = None
